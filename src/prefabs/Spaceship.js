fd8319f0af6497feeec62204668addc8045ce557
Brannon Eakles <beakles@ucsc.edu>
2023-04-24 06:44:48 -0700

    Updating files

--- src/prefabs/Spaceship.js
+++ src/prefabs/Spaceship.js
@@ -2,10 +2,10 @@ class Spaceship extends Phaser.GameObjects.Sprite {
   constructor(scene, x, y, texture, frame, pointValue) {
     super(scene, x, y, texture, frame);
 
-    scene.add.existing(this);    // add to existing scene
+    scene.add.existing(this);                              // add to existing scene
     
-    this.points = pointValue;    // store pointValue
-    this.moveSpeed = 3;         // pixels per frame
+    this.points = pointValue;                              // store pointValue
+    this.moveSpeed = game.settings.spaceshipSpeed;         // pixels per frame
   }
 
   update() {
