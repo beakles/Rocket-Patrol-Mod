519f44fb249baa344263d37ebcaeaadbe601aa24
Brannon Eakles <beakles@ucsc.edu>
2023-04-24 06:45:58 -0700

    Updating files

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -1,3 +1,5 @@
+// Code adapted from https://pre83.com/CMPM120/Project%2001%20%20Phaser%203%20Rocket%20Patrol%20Tutorial.html
+
 // Rocket prefab
 class Rocket extends Phaser.GameObjects.Sprite {
   constructor(scene, x, y, texture, frame) {
--- src/prefabs/Spaceship.js
+++ src/prefabs/Spaceship.js
@@ -1,3 +1,5 @@
+// Code adapted from https://pre83.com/CMPM120/Project%2001%20%20Phaser%203%20Rocket%20Patrol%20Tutorial.html
+
 class Spaceship extends Phaser.GameObjects.Sprite {
   constructor(scene, x, y, texture, frame, pointValue) {
     super(scene, x, y, texture, frame);
